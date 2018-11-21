da76283a15e7776220b29d3ef4d66494303777e8
Laharivavilala <44664439+Laharivavilala@users.noreply.github.com>
2018-11-21 18:23:11 +0530

    Add files via upload

---
+++ Hibernate_project/src/com/capg/paymentwallet/dao/AccountDAOImpl.java
@@ -0,0 +1,93 @@
+package com.capg.paymentwallet.dao;
+
+import javax.persistence.EntityManager;
+
+import com.capg.paymentwallet.bean.AccountBean;
+
+public class AccountDAOImpl implements IAccountDao {
+
+	
+	EntityManager em;
+	
+	@Override
+	public boolean createAccount(AccountBean accountBean) throws Exception {
+		try{
+			
+			this.em=JPAManager.createEntityManager();
+			em.getTransaction().begin();
+			
+			em.persist(accountBean);
+			
+			em.getTransaction().commit();
+			JPAManager.closeResources(em);
+			
+			return true;
+		}catch(Exception e){
+
+			return false;
+		}
+	
+	}
+
+	@Override
+	public boolean updateAccount(AccountBean accountBean) throws Exception {
+		try{
+			this.em=JPAManager.createEntityManager();
+			em.getTransaction().begin();
+			
+			em.merge(accountBean);
+			
+			em.getTransaction().commit();
+			JPAManager.closeResources(em);
+			return true;
+		}catch(Exception e){
+			e.printStackTrace();
+			return false;
+		}
+	
+	}
+	public boolean deposit(AccountBean accountBean, double depositAmount) throws Exception {
+		
+		accountBean.setBalance(accountBean.getBalance()+depositAmount);
+		
+		
+		return updateAccount(accountBean);
+		
+	}
+	
+	public boolean withDraw(AccountBean accountBean, double withdrawAmount)
+			throws Exception {
+		double balance=accountBean.getBalance()-withdrawAmount;
+		accountBean.setBalance(balance);
+		
+		return updateAccount(accountBean);
+	}
+
+	@Override
+	public AccountBean findAccount(int accountId) throws Exception {
+		try{
+			em=JPAManager.createEntityManager();
+			AccountBean accountBean2=em.find(AccountBean.class,accountId);
+			JPAManager.closeResources(em);
+			return accountBean2;
+			
+		}catch(Exception e){
+			return null;
+		}
+	}
+
+	@Override
+	public boolean fundTransfer(AccountBean transferingAccountBean,
+			AccountBean beneficiaryAccountBean, double transferAmount)
+			throws Exception {
+		transferingAccountBean.setBalance(transferingAccountBean.getBalance()-transferAmount);
+		beneficiaryAccountBean.setBalance(beneficiaryAccountBean.getBalance()+transferAmount);
+		
+		boolean result1=updateAccount(transferingAccountBean);
+		boolean result2=updateAccount(beneficiaryAccountBean);
+		return result1 && result2;
+	}
+
+	
+
+}
---
+++ Hibernate_project/src/com/capg/paymentwallet/service/AccountServiceImpl.java
@@ -0,0 +1,105 @@
+package com.capg.paymentwallet.service;
+
+import com.capg.paymentwallet.bean.AccountBean;
+import com.capg.paymentwallet.bean.CustomerBean;
+import com.capg.paymentwallet.dao.AccountDAOImpl;
+import com.capg.paymentwallet.dao.IAccountDao;
+import com.capg.paymentwallet.exception.CustomerException;
+import com.capg.paymentwallet.exception.CustomerExceptionMessage;
+
+public class AccountServiceImpl implements IAccountService {
+
+	@Override
+	public boolean createAccount(AccountBean accountBean) throws Exception {
+		boolean result = false;
+		if (validate(accountBean) == true) {
+			IAccountDao dao = new AccountDAOImpl();
+			result = dao.createAccount(accountBean);
+		}
+
+		return result;
+	}
+
+	@Override
+	public boolean deposit(AccountBean accountBean, double depositAmount)
+			throws Exception {
+
+		IAccountDao dao = new AccountDAOImpl();
+		return dao.deposit(accountBean, depositAmount);
+	}
+
+	@Override
+	public boolean withdraw(AccountBean accountBean, double withdrawAmount)
+			throws Exception {
+
+		IAccountDao dao = new AccountDAOImpl();
+		return dao.withDraw(accountBean, withdrawAmount);
+	}
+
+	@Override
+	public boolean fundTransfer(AccountBean transferingAccountBean,
+			AccountBean beneficiaryAccountBean, double transferAmount)
+			throws Exception {
+		IAccountDao dao = new AccountDAOImpl();
+
+		return dao.fundTransfer(transferingAccountBean, beneficiaryAccountBean,
+				transferAmount);
+	}
+
+	@Override
+	public AccountBean findAccount(int accountId) throws Exception {
+		IAccountDao dao = new AccountDAOImpl();
+		AccountBean bean = dao.findAccount(accountId);
+		return bean;
+	}
+
+	public boolean validate(AccountBean accountBean) throws CustomerException {
+
+		boolean isValid = true;
+		if (!(accountBean.getCustomerBean().getFirstName()
+				.matches("[a-zA-Z]{4,}"))) {
+			// isValid=false;
+			throw new CustomerException(CustomerExceptionMessage.fNameERROR);
+		}
+		if (!(accountBean.getCustomerBean().getLastName()
+				.matches("[a-zA-Z]{4,}"))) {
+			// isValid=false;
+			throw new CustomerException(CustomerExceptionMessage.lNameERROR);
+		}
+		if (!(accountBean.getCustomerBean().getPhoneNo().toString()
+				.matches("^[6-9][0-9]{9}"))) {
+
+			// isValid=false;
+			throw new CustomerException(CustomerExceptionMessage.phoneNumERROR);
+		}
+		if ((accountBean.getCustomerBean().getEmailId() == null || !(accountBean
+				.getCustomerBean().getEmailId()
+				.matches("[a-zA-Z][a-zA-z0-9-.]*@[a-zA-Z0-9]+([.][a-zA-Z)]+)+")))) {
+
+			// isValid=false;
+			throw new CustomerException(CustomerExceptionMessage.mailIdERROR);
+		}
+		if ((accountBean.getCustomerBean().getPanNum() == null)
+				|| (!(accountBean.getCustomerBean().getPanNum()
+						.matches("^[A-Z][A-Z0-9]{9}")))) {
+
+			// isValid=false;
+			throw new CustomerException(CustomerExceptionMessage.panERROR);
+		}
+		if ((accountBean.getCustomerBean().getAddress() == null)
+				|| (!(accountBean.getCustomerBean().getAddress()
+						.matches("[A-Za-z0-9]{5,50}")))) {
+
+			throw new CustomerException(CustomerExceptionMessage.addressERROR);
+		}
+		if (accountBean.getBalance() == 0 || !(accountBean.getBalance() > 0)) {
+
+			throw new CustomerException(CustomerExceptionMessage.balanceERROR);
+
+		}
+
+		return isValid;
+
+	}
+
+}
